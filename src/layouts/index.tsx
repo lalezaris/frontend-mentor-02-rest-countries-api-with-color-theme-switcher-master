4cc410f27bb4eff4e3f1eae5e0af44d2637f1dcf
Sam Lalezari <sam.lalezari@gmail.com>
2023-10-11 18:53:09 -0400

    Add env toggle to theme switcher

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -10,7 +10,9 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <>
       <header>
         <h1>Where in the world?</h1>
-        <div className="theme-toggle">Dark Mode</div>
+        {process.env.NODE_ENV === "development" && (
+          <div className="theme-toggle">Dark Mode</div>
+        )}
       </header>
       <main>{children}</main>
     </>
